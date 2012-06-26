b56a514cfb4fafb29d073c88920a3630040597f3
Colin Hebert <colin.hebert@oucs.ox.ac.uk>
2012-06-26 15:55:44 +0100

    Add documentation to ContentProducer

--- api/src/main/java/uk/ac/ox/oucs/search2/content/ContentProducer.java
+++ api/src/main/java/uk/ac/ox/oucs/search2/content/ContentProducer.java
@@ -1,15 +1,23 @@
 package uk.ac.ox.oucs.search2.content;
 
 /**
+ * Generate {@link Content} elements based on a reference
+ *
  * @author Colin Hebert
  */
 public interface ContentProducer {
+    /**
+     * Create the appropriate {@link Content} for the given reference
+     *
+     * @param reference Reference for a specific element in sakai
+     * @return a content object containing the actual data and metadata associated
+     */
     Content getContent(String reference);
 
     /**
-     * Check if the given reference references handled content
+     * Check if the given reference can be read by the current producer
      *
-     * @param reference Content to check
+     * @param reference Reference to check
      * @return true if the content is handled by the current ContentProducer, false otherwise
      */
     boolean isHandled(String reference);
