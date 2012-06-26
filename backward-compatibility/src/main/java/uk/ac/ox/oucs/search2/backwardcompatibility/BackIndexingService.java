608392f37a5b8e089cd20c0d7658d431c77cd229
Colin Hebert <colin.hebert@oucs.ox.ac.uk>
2012-06-26 15:23:23 +0100

    Create a BackwardCompatible IndexingService

---
+++ backward-compatibility/src/main/java/uk/ac/ox/oucs/search2/backwardcompatibility/BackIndexingService.java
@@ -0,0 +1,66 @@
+package uk.ac.ox.oucs.search2.backwardcompatibility;
+
+import org.sakaiproject.search.api.SearchIndexBuilder;
+import uk.ac.ox.oucs.search2.AbstractIndexingService;
+import uk.ac.ox.oucs.search2.content.Content;
+
+/**
+ * @author Colin Hebert
+ */
+public class BackIndexingService extends AbstractIndexingService {
+    private SearchIndexBuilder searchIndexBuilder;
+
+    @Override
+    public void indexContent(String eventHandlerName, Iterable<Content> content) {
+        throw new UnsupportedOperationException("The previous search service doesn't support manual indexation");
+    }
+
+    @Override
+    public void unindexContent(String eventHandlerName, Iterable<Content> content) {
+        throw new UnsupportedOperationException("The previous search service doesn't support manual removal");
+    }
+
+    @Override
+    public void indexSiteTool(String eventHandlerName, Iterable<Content> content, String siteTool) {
+        throw new UnsupportedOperationException("The previous search service doesn't support manual tool indexation");
+    }
+
+    @Override
+    public void unindexSiteTool(String eventHandlerName, String siteTool) {
+        throw new UnsupportedOperationException("The previous search service doesn't support manual tool removal");
+    }
+
+    @Override
+    public void indexSite(String eventHandlerName, Iterable<Content> content, String site) {
+        searchIndexBuilder.refreshIndex(site);
+    }
+
+    @Override
+    public void reindexSite(String eventHandlerName, Iterable<Content> content, String site) {
+        searchIndexBuilder.rebuildIndex(site);
+    }
+
+    @Override
+    public void unindexSite(String eventHandlerName, String site){
+        throw new UnsupportedOperationException("The previous search service doesn't support manual site removal");
+    }
+
+    @Override
+    public void indexAll(String eventHandlerName, Iterable<Content> content) {
+        searchIndexBuilder.refreshIndex();
+    }
+
+    @Override
+    public void reindexAll(String eventHandlerName, Iterable<Content> content) {
+        searchIndexBuilder.rebuildIndex();
+    }
+
+    @Override
+    public void unindexAll(String eventHandlerName) {
+        throw new UnsupportedOperationException("The previous search service doesn't support manual index cleaning removal");
+    }
+
+    public void setSearchIndexBuilder(SearchIndexBuilder searchIndexBuilder) {
+        this.searchIndexBuilder = searchIndexBuilder;
+    }
+}
