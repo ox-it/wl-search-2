f1c398c9bd018b950160bbb9ccfbb40da9a4dc61
Colin Hebert <colin.hebert@oucs.ox.ac.uk>
2012-07-09 14:05:04 +0100

    New file containing only constants for solr search

---
+++ solr/src/main/java/uk/ac/ox/oucs/search2/solr/SolrSchemaConstants.java
@@ -0,0 +1,9 @@
+package uk.ac.ox.oucs.search2.solr;
+
+/**
+ * @author Colin Hebert
+ */
+public final class SolrSchemaConstants {
+    public static final String CONTENT_FIELD = "contents";
+    public static final String CONTEXT_FIELD = "context";
+}
